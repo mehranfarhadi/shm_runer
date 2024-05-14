e06202e376b9499eecb384462469e46b8af6846e
mehran <farhadimehran479@gmail.com>
2024-05-14 15:05:38 +0330

    add c code example

---
+++ example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// A simple function that returns a string
+const char* example_function() {
+    print();
+    return "Hello from C!";
+}
+
+cont void print() {
+    printf("hello world!!!");
+}
